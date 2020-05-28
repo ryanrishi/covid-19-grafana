3dad4243375e8cbba49c6519a6dcf6fb4f8138b7
Ryan Rishi <ryan@ryanrishi.com>
2020-05-27 17:38:37 -0700

    change ingress port from 80 → 3000

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,8 +32,8 @@ resource "aws_security_group" "security_group" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
