7e1b9680ea2172461f9b6d1795a892d18b46d603
Ryan Rishi <ryan@ryanrishi.com>
2020-05-27 21:13:13 -0700

    clone covid-19 git repository

--- terraform/files/init.sh
+++ terraform/files/init.sh
@@ -10,3 +10,8 @@ sudo usermod -a -G docker ec2-user
 # Download Docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo yum install -y git
+cd /opt
+git clone https://github.com/ryanrishi/covid-19-grafana.git
+cd covid-19-grafana
