ffa759b859b5188898707d5349f56a7c630312b3
Ryan Rishi <ryan@ryanrishi.com>
2020-05-27 17:38:24 -0700

    create DNS hostname for VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,8 @@ terraform {
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "subnet" {
