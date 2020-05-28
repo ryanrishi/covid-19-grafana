b66b80a5b55c93e76499c8490ed4ff1a812ebfdc
Ryan Rishi <ryan@ryanrishi.com>
2020-05-27 21:13:47 -0700

    import COVID-19 CSV into InfluxDB

--- terraform/files/init.sh
+++ terraform/files/init.sh
@@ -20,3 +20,14 @@ GF_SERVER_DOMAIN=stage.ryanrishi.com \
 GF_SERVER_SERVE_FROM_SUB_PATH=true \
 GF_SERVER_ROOT_URL=https://stage.ryanrishi.com/covid-19-dashboard/ \
 docker-compose up -d
+
+# Download latest COVID-19 data
+cd /tmp
+curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv -o us-counties.csv
+
+curl https://raw.githubusercontent.com/fabio-miranda/csv-to-influxdb/master/csv-to-influxdb.py -o csv-to-influxdb.py
+sudo yum install -y python-pip
+pip install --user pytz influxdb
+
+# Import CSV into InfluxDB
+python csv-to-influxdb.py -i us-counties.csv --dbname covid-19 --create --timecolumn date --timeformat '%Y-%m-%d' --tagcolumns county,state,fips --fieldcolumns cases,deaths
